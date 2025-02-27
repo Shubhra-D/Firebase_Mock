71204b9ddf50699ca74f91580f5d14f6158fbc1d
Shubhra Dwivedi <shubhradwivedi21@gmail.com>
2025-02-27 17:24:18 +0530

    updating thunk

--- Redux_MOCK2/src/Redux/store.js
+++ Redux_MOCK2/src/Redux/store.js
@@ -1,10 +1,11 @@
-import {combineReducers, legacy_createStore as createStore} from 'redux'
+import {applyMiddleware, combineReducers, legacy_createStore as createStore} from 'redux'
 import authReducer from '../Redux/authReducer'
 import notesReducer from '../Redux/notesReducer'
+import {thunk} from 'redux-thunk'
 
 const rootReducer = combineReducers({
     auth:authReducer,
     notes:notesReducer,
 });
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer,applyMiddleware(thunk))
